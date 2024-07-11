6c0a59a0e5e8a7386d199927beced097e09d47a9
Dennis Indorsky <dennis.indorsky@hp.com>
2024-07-11 13:46:37 +0300

    work in progress

--- LCode/WhenTesting_SubstringWithConcatenationOfAllWords.cs
+++ LCode/WhenTesting_SubstringWithConcatenationOfAllWords.cs
@@ -1,5 +1,4 @@
-using System.Diagnostics;
-using System.Text;
+using System.Collections.Frozen;
 
 namespace LCode;
 
@@ -10,11 +9,13 @@ public class WhenTesting_SubstringWithConcatenationOfAllWords
     [InlineData(new[] { 0, 9 }, "barfoothefoobarman", new[] { "foo", "bar" })]
     [InlineData(new[] { 0, 3, 6 }, "foobarfoobar", new[] { "foo", "bar" })]
     [InlineData(new int[0], "wordgoodgoodgoodbestword", new[] { "word", "good", "best", "word" })]
-    [InlineData(new int[] { 6, 9, 12 }, "barfoofoobarthefoobarman", new[] { "bar", "foo", "the" })]
-    [InlineData(new int[] { 8 }, "wordgoodgoodgoodbestword", new[] { "word", "good", "best", "good" })]
-    [InlineData(new int[] { 13 }, "lingmindraboofooowingdingbarrwingmonkeypoundcake", new[] { "fooo", "barr", "wing", "ding", "wing" })]
-    [InlineData(new int[] { 1, 3 }, "abaababbaba", new[] { "ab", "ba", "ab", "ba" })]
-    [InlineData(new int[] { 1, 3 }, "abaababbaba", new[] { "dhvf", "sind", "ffsl", "yekr", "zwzq", "kpeo", "cila", "tfty", "modg", "ztjg", "ybty", "heqg", "cpwo", "gdcj", "lnle", "sefg", "vimw", "bxcb" })]
+    [InlineData(new[] { 6, 9, 12 }, "barfoofoobarthefoobarman", new[] { "bar", "foo", "the" })]
+    [InlineData(new[] { 8 }, "wordgoodgoodgoodbestword", new[] { "word", "good", "best", "good" })]
+    [InlineData(new[] { 13 }, "lingmindraboofooowingdingbarrwingmonkeypoundcake", new[] { "fooo", "barr", "wing", "ding", "wing" })]
+    [InlineData(new[] { 1, 3 }, "abaababbaba", new[] { "ab", "ba", "ab", "ba" })]
+    [InlineData(new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, "aaaaaaaaaaaaaa", new[] { "aa", "aa" })]
+
+
     public void TestIt(int[] expected, string s, string[] words)
     {
         var res = FindSubstring(s, words);
@@ -24,148 +25,126 @@ public class WhenTesting_SubstringWithConcatenationOfAllWords
 
     }
 
-
-
-    [Theory]
-    [InlineData(new[] { "12", "21" }, new[] { 1, 2 })]
-    [InlineData(new[] { "12", "21" }, new[] { 1, 2, 3 })]
-    [InlineData(new[] { "12", "21" }, new[] { 1, 2, 3, 4 })]
-    [InlineData(new[] { "12", "21" }, new[] { 1, 2, 3, 4, 5 })]
-    public void FindPermutations(string[] expected, int[] nums)
+    [Fact]
+    public void TestFromFileData()
     {
+        const string dataFile = @"SubstringWithConcatenationOfAllWords.txt";
+        var lines = File.ReadAllLines(dataFile);
 
+        string s = String.Empty;
+        string[] words = [];
 
-        int Factotrial(int n)
+        foreach (var line in lines)
         {
-            int sum = 1;
-            for (int i = 1; i <= n; ++i)
-                sum *= i;
-            return sum;
-        }
-
-        int f = Factotrial(nums.Length);
-        var tbl = FindPerm(nums);
-        Assert.Equal(f, tbl.Count);
-
-        var sb = new StringBuilder();
-        foreach (var row in tbl)
-        {
-            foreach (var r in row)
+            if (line.Contains("[String]"))
             {
-                sb.AppendFormat("{0} ", r);
+                s = line.Replace("[String]", "");
             }
-
-            sb.AppendLine();
+            else if (line.Contains("[Words]"))
+            {
+                words = line.Replace("[Words]", "")
+                  .Replace("[", "")
+                  .Replace("]", "").Split(',', StringSplitOptions.RemoveEmptyEntries);
+            }
         }
+        var res = FindSubstring(s, words);
 
-        string dump = sb.ToString();
     }
 
 
 
-    private List<List<int>> FindPerm(int[] nums)
+    private int FindSubstringInternal(string s, Dictionary<string, int> testMap, int wordLen, int startIdx)
     {
-        List<List<int>> result = new();
-        BackTrack(result, nums, 0, nums.Length);
-        return result;
-    }
-    private void BackTrack(List<List<int>> result, int[] array, int start, int end)
-    {
-        if (start == end)
-        {
-            result.Add([.. array]);
-        }
-        else
-        {
-            for (int i = start; i < end; i++)
-            {
-                (array[start], array[i]) = (array[i], array[start]);
-                BackTrack(result, array, start + 1, end);
-                (array[start], array[i]) = (array[i], array[start]);
-            }
-        }
 
-    }
+        var map = new Dictionary<string, int>(testMap);
 
-    private IReadOnlyList<string> ConcantinatedWords(string[] words)
-    {
-        var hash = new HashSet<string>();
-        int[] idxs = new int[words.Length];
-        for (int i = 0; i < idxs.Length; ++i)
-            idxs[i] = i;
+        bool firstFound = false;
+        int result = -1;
+        int chIdx = 0;
+        var span = s.AsSpan(startIdx);
+        var span2 = s.AsSpan(startIdx);
 
-        var permIdx = FindPerm(idxs);
-        foreach (var item in permIdx)
-        {
-            var sb = new StringBuilder();
-            foreach (var idx in item)
-            {
-                sb.Append(words[idx]);
-            }
-            hash.Add(sb.ToString());
-        }
-        return hash.ToArray();
-    }
-
-    private int FindMatch(Span<char> s, Span<char> pattern, int singleWordLen, int fromIdx = 0)
-    {
-        if (s.Length <= fromIdx)
-            return -1;
-
-
-        s = s.Slice(fromIdx);
-
-        if (s.Length < pattern.Length)
-            return -1;
-
-        var tmpptrn = pattern;
-        var tmps = s;
-
-        int res = -1;
-        int idx = 0;
-        int cnt = 0;
-        while (!tmpptrn.IsEmpty && !tmps.IsEmpty)
-        {
-            if (tmps[0] == tmpptrn[0])
-            {
-                if (res == -1)
-                    res = idx;
-                tmpptrn = tmpptrn.Slice(1);
-            }
-            else if (res != -1)
-            {
-                cnt++;
-                tmps = s.Slice(/*singleWordLen **/ cnt);
-                res = -1;
-                tmpptrn = pattern;
-                idx = s.Length - tmps.Length;
-                continue;
-
-            }
-            tmps = tmps.Slice(1);
-            idx++;
-        }
-
-        return tmpptrn.IsEmpty ? res + fromIdx : -1;
-    }
-
-    public IList<int> FindSubstring(string s, string[] words)
-    {
-        var permutations = ConcantinatedWords(words);
-        var res = new List<int>();
-
-        foreach (var permutation in permutations)
+        while (span2.Length >= wordLen)
         {
             
-            var idx = FindMatch(s.ToCharArray(), permutation.ToCharArray(), words[0].Length);
-
-            while (idx != -1)
+            var key = new string(span2.Slice(0, wordLen));
+            if (map.ContainsKey(key) && map[key] > 0)
             {
-                res.Add(idx);
-                idx = FindMatch(s.ToCharArray(), permutation.ToCharArray(), words[0].Length, idx + 1);
+
+                if (!firstFound)
+                {
+                    firstFound = true;
+                    result = span.Length - span2.Length + startIdx;
+                }
+
+                span2 = span2.Slice(wordLen);
+
+                map[key]--;
+                if (IsMatchFound())
+                    break;
+            }
+            else
+            {
+                chIdx++;
+                if (firstFound)
+                {
+                    map = testMap.ToDictionary();
+                    span2 = s.AsSpan(startIdx + chIdx);
+                }
+                else
+                {
+                    span2 = span2.Slice(1);
+                }
+                firstFound = false;
             }
         }
+
+        return IsMatchFound() ? result : -1;
+
+        bool IsMatchFound()
+        {
+            foreach (var n in map)
+            {
+                if (n.Value > 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+    public IList<int> FindSubstring(string s, string[] words)
+    {
+        Dictionary<string, int> CreateMap()
+        {
+            var map = new Dictionary<string, int>();
+            foreach (var word in words)
+            {
+                if (!map.ContainsKey(word))
+                    map.Add(word, 0);
+
+                map[word]++;
+            }
+
+            return map;
+        }
+
+
+
+        var testMap = CreateMap();
+
+        var res = new List<int>();
+
+        int wordLen = words[0].Length;
+
+
+        int idx = FindSubstringInternal(s, testMap, wordLen, 0);
+        while (idx != -1)
+        {
+            res.Add(idx);
+            idx = FindSubstringInternal(s, testMap, wordLen, idx + 1);
+        }
+
 
         return res;
     }
+
 }
